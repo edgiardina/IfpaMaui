9fdd545f7b84238f7d2506a970f231a6a3e4c095
Ed Giardina <ed@edgiardina.com>
2022-07-20 22:24:05 -0400

    Adding rules page

---
+++ Views/RulesPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Ifpa.ViewModels;
+using System;
+using System.Collections;
+using Microsoft.Maui;
+
+
+namespace Ifpa.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RulesPage : ContentPage
+    {
+        //RulesViewModel viewModel;        
+
+        public RulesPage()
+        {
+            InitializeComponent();
+
+            //BindingContext = this.viewModel = new RulesViewModel();
+            //viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            RulesWebView.Source = "https://docs.google.com/viewer?embedded=true&url=https://www.ifpapinball.com/wp/wp-content/uploads/2021/04/PAPA_IFPA-Complete-Competition-Rules-2021.04.06.pdf";
+        }
+    }
+}
