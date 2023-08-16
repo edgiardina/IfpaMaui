ba425ae6b4146d7b14137a627cf0f7b0d64d323a
Ed Giardina <ed@edgiardina.com>
2023-08-15 20:34:19 -0400

    Remove Syncfusion charts for LiveCharts2

--- ViewModels/PlayerDetailViewModel.cs
+++ ViewModels/PlayerDetailViewModel.cs
@@ -1,8 +1,13 @@
 ﻿using Ifpa.Models;
+using LiveChartsCore;
+using LiveChartsCore.Kernel;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
 using PinballApi;
 using PinballApi.Extensions;
 using PinballApi.Models.v2.WPPR;
 using PinballApi.Models.WPPR.v2.Players;
+using SkiaSharp;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 
@@ -15,9 +20,12 @@ namespace Ifpa.ViewModels
         public Command LoadItemsCommand { get; set; }
         public Command PostPlayerLoadCommand { get; set; }
 
+
         public int PlayerId { get; set; }
         public int LastTournamentCount { get; set; }
         private Player playerRecord = new Player { PlayerStats = new PinballApi.Models.WPPR.v1.Players.PlayerStats { }, ChampionshipSeries = new List<ChampionshipSeries> { } };
+
+        private static readonly int s_logBase = 10;
 
         public Player PlayerRecord
         {
@@ -28,9 +36,29 @@ namespace Ifpa.ViewModels
                 OnPropertyChanged(null);
             }
         }
-        public ObservableCollection<RankHistory> PlayerRankHistory { get; set; }
+        public List<ISeries> PlayerRankHistoryLineSeries { get; set; } = new List<ISeries>();
 
-        public ObservableCollection<RatingHistory> PlayerRatingHistory { get; set; }
+        public List<ISeries> PlayerRatingHistoryLineSeries { get; set; } = new List<ISeries>();
+
+        public Axis[] TimeAxis { get; set; } =
+        {
+            new Axis
+            {
+                Labeler = value => value.AsDate().ToString("yyyy"),
+                UnitWidth = TimeSpan.FromDays(365.25).Ticks,
+                MinStep = TimeSpan.FromDays(365.25).Ticks
+            }
+        };
+
+        public Axis[] RankAxis { get; set; } =
+        {
+            new Axis
+            {
+                MaxLimit = 50_000,
+                MinLimit = 1,
+                Labeler = value => Math.Pow(s_logBase, value).ToString("F0")                
+            }
+        };
 
         public string Name => PlayerRecord.FirstName + " " + PlayerRecord.LastName;
 
@@ -69,7 +97,7 @@ namespace Ifpa.ViewModels
         public int TotalEvents => PlayerRecord.PlayerStats.TotalEventsAllTime;
 
         public int TotalActiveEvents => PlayerRecord.PlayerStats.TotalActiveEvents;
-        
+
         public int EventsOutsideCountry => PlayerRecord.PlayerStats.TotalEventsAway;
 
         public string PlayerAvatar
@@ -91,17 +119,21 @@ namespace Ifpa.ViewModels
         public string Location => $"{PlayerRecord.City}{(!string.IsNullOrEmpty(PlayerRecord.City) && !string.IsNullOrEmpty(PlayerRecord.StateProvince) ? "," : string.Empty)} {PlayerRecord.StateProvince} {PlayerRecord.CountryName}".Trim().Replace("  ", " ");
 
         public bool IsRegistered => PlayerRecord.IfpaRegistered;
-        
+
         public PlayerDetailViewModel(PinballRankingApiV1 pinballRankingApiV1, PinballRankingApiV2 pinballRankingApiV2, AppSettings appSettings) : base(pinballRankingApiV1, pinballRankingApiV2)
         {
-            PlayerRankHistory = new ObservableCollection<RankHistory>();
-            PlayerRatingHistory = new ObservableCollection<RatingHistory>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
             AppSettings = appSettings;
         }
 
         public async Task ExecuteLoadItemsCommand()
         {
+            Color resourceColor = null;
+            if(App.Current.Resources.TryGetValue("IconAccentColor", out var color))
+            {
+                resourceColor = color as Color;
+            };       
+
             try
             {
                 if (PlayerId > 0)
@@ -110,14 +142,38 @@ namespace Ifpa.ViewModels
                     var playerData = await PinballRankingApiV2.GetPlayer(PlayerId);
                     var playerHistoryData = await PinballRankingApiV2.GetPlayerHistory(PlayerId);
 
-                    if(playerHistoryData.RankHistory != null)
-                        PlayerRankHistory = new ObservableCollection<RankHistory>(playerHistoryData.RankHistory);
+                    var playerRankSeries = new LineSeries<RankHistory>
+                    {
+                        Values = playerHistoryData.RankHistory,
+                        Fill = null,
+                        GeometryFill = null,
+                        GeometryStroke = null,
+                        Stroke = new SolidColorPaint(SKColor.Parse(resourceColor.ToHex())) { StrokeThickness = 2 },
+                        Mapping = (history, point) =>
+                        {
+                            point.Coordinate = new Coordinate(history.RankDate.Ticks, Math.Log(history.RankPosition, s_logBase));
+                        }
+                    };
+                    PlayerRankHistoryLineSeries.Add(playerRankSeries);
 
-                    if (playerHistoryData.RatingHistory != null)
-                        PlayerRatingHistory = new ObservableCollection<RatingHistory>(playerHistoryData.RatingHistory);
 
+                    var playerRatingSeries = new LineSeries<RatingHistory>
+                    {
+                        Values = playerHistoryData.RatingHistory,
+                        Fill = null,
+                        GeometryFill = null,
+                        GeometryStroke = null,
+                        Stroke = new SolidColorPaint(SKColor.Parse(resourceColor.ToHex())) { StrokeThickness = 2 },
+                        Mapping = (history, point) =>
+                        {
+                            point.Coordinate = new Coordinate(history.RatingDate.Ticks, history.Rating);
+                        }
+                    };
+                    PlayerRatingHistoryLineSeries.Add(playerRatingSeries);
+
+                    OnPropertyChanged();
                     PlayerRecord = playerData;
-                 
+
                     AddPlayerToAppLinks();
 
                     if (PostPlayerLoadCommand != null)
@@ -126,7 +182,7 @@ namespace Ifpa.ViewModels
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
             }
@@ -140,7 +196,7 @@ namespace Ifpa.ViewModels
         {
             var results = await PinballRankingApi.GetPlayerResults(playerId);
 
-            foreach(var result in results.Results) 
+            foreach (var result in results.Results)
             {
                 var record = new ActivityFeedItem
                 {
@@ -174,7 +230,7 @@ namespace Ifpa.ViewModels
             {
                 Application.Current.AppLinks.RegisterLink(entry);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
             }
--- ViewModels/StatsViewModel.cs
+++ ViewModels/StatsViewModel.cs
@@ -3,16 +3,31 @@ using System.Diagnostics;
 using PinballApi.Models.WPPR.v1.Statistics;
 using Ifpa.Models;
 using PinballApi;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
 
 namespace Ifpa.ViewModels
 {
 
     public class StatsViewModel : BaseViewModel
     {
-        public ObservableCollection<PlayersByCountryStat> PlayersByCountry { get; set; }
-        public ObservableCollection<EventsByYearStat> EventsByYear { get; set; }
 
-        public ObservableCollection<PlayersByYearStat> PlayersByYear { get; set; }
+        public List<ISeries> PlayersByCountrySeries { get; set; } = new List<ISeries>();
+
+        public List<ISeries> EventsByYearSeries { get; set; } = new List<ISeries>();
+
+        public List<Axis> EventsByYearAxis { get; set; } = new List<Axis>();
+
+        public List<ISeries> PlayersByYearSeries { get; set; } = new List<ISeries> {
+            new ColumnSeries<int>
+            {
+                    Name = "Players" 
+            }
+        };
+
+        public List<Axis> PlayersByYearAxis { get; set; } = new List<Axis>();
 
         public ObservableCollectionRange<PointsThisYearStat> MostPointsPlayers { get; set; }
 
@@ -25,9 +40,6 @@ namespace Ifpa.ViewModels
         public StatsViewModel(PinballRankingApiV1 pinballRankingApiV1, PinballRankingApiV2 pinballRankingApiV2) : base(pinballRankingApiV1, pinballRankingApiV2)
         {
             Title = "Stats";
-            PlayersByCountry = new ObservableCollection<PlayersByCountryStat>();
-            EventsByYear = new ObservableCollection<EventsByYearStat>();
-            PlayersByYear = new ObservableCollection<PlayersByYearStat>();
             MostPointsPlayers = new ObservableCollectionRange<PointsThisYearStat>();
             MostEventsPlayers = new ObservableCollectionRange<MostEventsStat>();
             BiggestMovers = new ObservableCollectionRange<BiggestMoversStat>();
@@ -44,30 +56,65 @@ namespace Ifpa.ViewModels
 
             try
             {
-                PlayersByCountry.Clear();
-                EventsByYear.Clear();
-                PlayersByYear.Clear();
                 MostPointsPlayers.Clear();
                 MostEventsPlayers.Clear();
                 BiggestMovers.Clear();
 
                 var playersByCountry = await PinballRankingApi.GetPlayersByCountryStat();
-                foreach (var item in playersByCountry)
+
+                var groupedStats = playersByCountry.GroupBy(
+                    stat => stat.Count < 100 ? "Other" : stat.CountryName,
+                    (key, group) => new PlayersByCountryStat
+                    {
+                        Count = key == "Other" ? group.Sum(item => item.Count) : group.First().Count,
+                        CountryName = key
+                    });
+
+                foreach (var item in groupedStats)
                 {
-                    PlayersByCountry.Add(item);
+                    var series = new PieSeries<int>
+                    {
+                        Values = new List<int> { item.Count },
+                        Name = item.CountryName
+                    };
+
+                    PlayersByCountrySeries.Add(series);
                 }
 
                 var eventsByYear = await PinballRankingApi.GetEventsPerYearStat();
-                foreach (var item in eventsByYear)
+
+                var eventsByYearColumnSeries = new ColumnSeries<int>
                 {
-                    EventsByYear.Add(item);
-                }
+                    Name = "Events",
+                    Values = eventsByYear.OrderBy(x => x.Year).Select(x => x.Count).ToArray()
+                };
+
+                var eventsByYearAxis = new Axis
+                {
+                    Labels = eventsByYear.OrderBy(x => x.Year).Select(x => x.Year.ToString()).ToList(),
+                    LabelsRotation = 0,
+                    SeparatorsAtCenter = false,
+                    TicksAtCenter = true
+                };
+
+                EventsByYearSeries.Add(eventsByYearColumnSeries);
+                EventsByYearAxis.Add(eventsByYearAxis);
 
                 var playersByYear = await PinballRankingApi.GetPlayersPerYearStat();
-                foreach (var item in playersByYear)
+
+                PlayersByYearSeries[0].Values = playersByYear.OrderBy(x => x.Year).Select(x => x.Count).ToArray();           
+
+                var playersByYearAxis = new Axis
                 {
-                    PlayersByYear.Add(item);
-                }
+                    Labels = playersByYear.OrderBy(x => x.Year).Select(x => x.Year.ToString()).ToList(),
+                    LabelsRotation = 0,
+                    SeparatorsAtCenter = false,
+                    TicksAtCenter = true,
+                };
+
+                PlayersByYearAxis.Add(playersByYearAxis);
+
+                OnPropertyChanged();
 
                 var mostPointsPlayers = await PinballRankingApi.GetPointsThisYearStats();
                 MostPointsPlayers.AddRange(mostPointsPlayers);
