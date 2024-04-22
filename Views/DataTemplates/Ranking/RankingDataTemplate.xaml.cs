055e9589c9bdb6a559dc47aac2fac984d5464cb8
Ed Giardina <ed@edgiardina.com>
2024-04-21 20:02:16 -0400

    Use Ranking Data Templates to preserve X:DataTypes

---
+++ Views/DataTemplates/Ranking/RankingDataTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Maui;
+
+
+namespace Ifpa.Views.DataTemplates.Ranking
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RankingDataTemplate : DataTemplate
+    {
+        public RankingDataTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
