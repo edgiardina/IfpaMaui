26dd2faca3514afdb35fdd792bfffd68a759dc2b
Ed Giardina <ed@edgiardina.com>
2025-05-12 22:24:23 -0400

    Remove badge behavior

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -1,7 +1,5 @@
 ﻿using Android.App;
 using Android.Runtime;
-using CommunityToolkit.Maui.ApplicationModel;
-using Ifpa.Platforms.Android;
 
 namespace Ifpa;
 
@@ -11,9 +9,6 @@ public class MainApplication : MauiApplication
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 		: base(handle, ownership)
 	{
-        var samsungProvider = new SamsungBadgeProvider();
-        BadgeFactory.SetBadgeProvider("com.sec.android.app.launcher", samsungProvider);
-        BadgeFactory.SetBadgeProvider("com.sec.android.app.twlauncher", samsungProvider);
     }
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
