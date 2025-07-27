ba4ce5d6fe078856d12ec17fc71e3a03bf86cbf6
Ed Giardina <ed@edgiardina.com>
2025-07-27 17:31:21 -0400

    Fix tournament behavior when tournament has no results

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,14 +1,15 @@
 ﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.OS;
 
 namespace Ifpa;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", 
+         MainLauncher = true, 
+         LaunchMode = LaunchMode.SingleTop,
+         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 [IntentFilter(new[] { Platform.Intent.ActionAppAction },
               Categories = new[] { Intent.CategoryDefault })]
-//TODO: Support Deep linking?
 //https://github.com/dotnet/maui/issues/11684
 [IntentFilter(new[] { Intent.ActionView },
           Categories = new[] {
