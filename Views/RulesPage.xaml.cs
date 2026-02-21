67af0827cfac9c82aba59edeac844f764f259dc6
Ed Giardina <ed@edgiardina.com>
2026-02-21 17:47:59 -0500

    use native rules viewer.

--- Views/RulesPage.xaml.cs
+++ Views/RulesPage.xaml.cs
@@ -1,26 +1,13 @@
-﻿using Ifpa.ViewModels;
-using System;
-using System.Collections;
-using Microsoft.Maui;
-
+using Ifpa.ViewModels;
 
 namespace Ifpa.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RulesPage : ContentPage
     {
-        private const string rulesPdfPath = "https://www.ifpapinball.com/wp/wp-content/uploads/2025/08/PAPA_IFPA-Complete-Competition-Rules-2025.08.pdf";
-
         public RulesPage()
         {
             InitializeComponent();
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            RulesWebView.Source = $"https://docs.google.com/viewer?embedded=true&url={rulesPdfPath}";
         }
     }
 }
